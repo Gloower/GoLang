1a753e2a0e01d951c04e4aba69d1472e36555d01
Gloower <gloowerzin@gmail.com>
2023-03-14 14:18:38 -0300

    Fazendo meus testes em Go

--- First.go
+++ First.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+/*
+Usar sempre fmt, por ser algo padrão do Go, o Print pode cair em desuso com o tempo.
+
+Funções parecidas com C.
+
+Palavras reservadas no GO podem causar erros na sintaxe
+*/
 
 func main() {
-	fmt.Println("Aprendendo go meu caro!")
+
+	d, eae := 32, 21
+	fmt.Println(d, eae)
+
+}
+
+//init sempre vai iniciar ao rodar o script, independente qual função estiver primeiro na ordem do código
+
+func init() {
+	eae, name := 90, "tudo certo"
+	fmt.Println(eae, name)
 }
